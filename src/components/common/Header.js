64d197d2d59158c23e925d3eae0e563b96021152
Ethan <ethan.nguyens@gmail.com>
2016-11-20 19:52:31 +0000

    add header component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
   <nav>
     <IndexLink to="/" activeClassName="active">Home</IndexLink>
     {" | "}
+    <Link to="/courses" activeClassName="active">Course</Link>
+    {" | "}
     <Link to="/about" activeClassName="active">About</Link>
   </nav>
   );
