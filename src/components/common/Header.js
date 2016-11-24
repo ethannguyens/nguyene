7f9edc602bac5b86048c7278bc7f529647d2b7c9
ethan.nguyen <ethannguyen@thehutgroup.com>
2016-11-24 10:54:28 +0000

    add loading dots and update header comp

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import {Link, IndexLink} from 'react-router';
+import LoadingDots from './LoadingDots';
 
 const Header = () => {
   return (
@@ -9,8 +10,13 @@ const Header = () => {
     <Link to="/courses" activeClassName="active">Course</Link>
     {" | "}
     <Link to="/about" activeClassName="active">About</Link>
+    {loading && <LoadingDots interval={100} dots={20} />}
   </nav>
   );
+};
+
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired
 };
 
 export default Header;
