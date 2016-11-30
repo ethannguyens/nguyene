c4c1fd1e878f783dece6807222e069498d0af531
ethan.nguyen <ethannguyen@thehutgroup.com>
2016-11-30 09:37:18 +0000

    add video component

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router';
+import Video from '../video/video';
 
 class HomePage extends React.Component {
   render() {
     return (
-      <div className="jumbotron">
-        <h1>Ethan Nguyen</h1>
-        <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+      <div className="homeBody">
+        <Video />
+        <div className="jumbotron">
+          <h1>Ethan Nguyen</h1>
+          <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+          <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+        </div>
       </div>
     );
   }
