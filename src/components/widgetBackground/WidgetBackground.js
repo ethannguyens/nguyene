7cbdade84f9e9159414f0155e375ff92831d2239
Ethan Nguyen <ethan.nguyens@gmail.com>
2016-12-01 13:39:32 +0000

    style widgetBackround video

--- src/components/widgetBackground/WidgetBackground.js
+++ src/components/widgetBackground/WidgetBackground.js
@@ -8,24 +8,24 @@ class WidgetBackground extends React.Component {
 
   render(){
     return (
-      <div className="widgetBackground">
+      <div className={this.props.class ? `widgetBackground ${this.props.class}` : `widgetBackground`}>
           <div className="widgetBackground_container">
             {this.props.vimeoId ? <iframe
               src={`https://player.vimeo.com/video/${this.props.vimeoId}?api=1&autoplay=1&loop=1&badge=0title=0&byline=0&portrait=0&background=1`}
               frameBorder="0" volume="0" allowFullScreen="1"/> : undefined}
 
             {this.props.youtubeId ? <iframe
-              src={`https://www.youtube.com/embed/${this.props.youtubeId}?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=${this.props.youtubeoId}`}
+              src={`https://www.youtube.com/embed/${this.props.youtubeId}?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=${this.props.youtubeId}`}
               frameBorder="0" volume="0" allowFullScreen="1"/> : undefined}
 
             {this.props.image ? <img src={this.props.image}/> : undefined}
           </div>
         <div className="widgetBackgroundContent">
-          {this.props.highlight ? <div className="widgetVideoContent_title">{this.props.highlight}</div> : undefined}
-          {this.props.title ? <div className="widgetVideoContent_subtitle">{this.props.title}</div> : undefined}
-          {this.props.subtitle ? <div className="widgetVideoContent_text">{this.props.subtitle}</div> : undefined}
-          {this.props.text ? <div className="widgetVideoContent_text">T{this.props.text} </div> : undefined}
-          {this.props.buttonText? <a className="button widgetVideoContent_text" href={this.props.buttonLink}>{this.props.buttonText}</a> : undefined}
+          {this.props.highlight ? <div className="highlight widgetBackgroundContent_highlight">{this.props.highlight}</div> : undefined}
+          {this.props.title ? <div className="title widgetBackgroundContent_title">{this.props.title}</div> : undefined}
+          {this.props.subtitle ? <div className="subtitle widgetBackgroundContent_subtitle">{this.props.subtitle}</div> : undefined}
+          {this.props.text ? <div className="text widgetBackgroundContent_text">T{this.props.text} </div> : undefined}
+          {this.props.buttonText? <a className="button widgetBackgroundContent_button" href={this.props.buttonLink}>{this.props.buttonText}</a> : undefined}
         </div>
       </div>
     );
@@ -33,12 +33,13 @@ class WidgetBackground extends React.Component {
 }
 
 WidgetBackground.propTypes = {
+  class: PropTypes.string,
   image: PropTypes.string,
   youtubeId: PropTypes.string,
   vimeoId: PropTypes.string,
   highlight: PropTypes.string,
   title: PropTypes.string,
-  subTitle: PropTypes.string,
+  subtitle: PropTypes.string,
   text: PropTypes.string,
   buttonText: PropTypes.string,
   buttonLink: PropTypes.string
