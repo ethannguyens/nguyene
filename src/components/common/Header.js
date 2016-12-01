4f96afb04aaaf254eb89093910a43907e3052b85
ethan.nguyen <ethannguyen@thehutgroup.com>
2016-12-01 20:08:45 +0000

    header css

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,17 +4,49 @@ import LoadingDots from './LoadingDots';
 
 const Header = () => {
   return (
-    <div className="header">
-      <nav className="header_nav">
-        <IndexLink className="header_navItem header_navItem-home" to="/" activeClassName="active">Home</IndexLink>
-        <Link className="header_navItem header_navItem-portfolio" to="/portfolio" activeClassName="active">Portfolio</Link>
-        <Link className="header_navItem header_navItem-education" to="/education" activeClassName="active">Education</Link>
-        <Link className="header_navItem header_navItem-about" to="/about" activeClassName="active">About</Link>
-        <Link className="header_navItem header_navItem-blog" to="/blog" activeClassName="active">Blog</Link>
-        <Link className="header_navItem header_navItem-resume" to="/resume" activeClassName="active">Resume</Link>
-        <Link className="header_navItem header_navItem-contact" to="/contact" activeClassName="active">Contact</Link>
-      </nav>
-    </div>
+    <nav>
+      <div id="logo">Your Logo here</div>
+      <label htmlFor="drop" className="toggle">Menu</label>
+      <input type="checkbox" id="drop"/>
+      <ul className="menu">
+        <li><a href="#">Home</a></li>
+        <li>
+          <label htmlFor="drop-1" className="toggle">WordPress +</label>
+          <a href="#">WordPress</a>
+          <input type="checkbox" id="drop-1"/>
+          <ul>
+            <li><a href="#">Themes and stuff</a></li>
+            <li><a href="#">Plugins</a></li>
+            <li><a href="#">Tutorials</a></li>
+          </ul>
+
+        </li>
+        <li>
+          <label htmlFor="drop-2" className="toggle">Web Design +</label>
+          <a href="#">Web Design</a>
+          <input type="checkbox" id="drop-2"/>
+          <ul>
+            <li><a href="#">Resources</a></li>
+            <li><a href="#">Links</a></li>
+            <li>
+              <label htmlFor="drop-3" className="toggle">Tutorials +</label>
+              <a href="#">Tutorials</a>
+              <input type="checkbox" id="drop-3"/>
+
+              <ul>
+                <li><a href="#">HTML/CSS</a></li>
+                <li><a href="#">jQuery</a></li>
+                <li><a href="#">Other</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li><a href="#">Graphic Design</a></li>
+        <li><a href="#">Inspiration</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </nav>
   );
 };
 
