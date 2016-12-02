59157c756db14282ea2d02592a9907c13bf025b7
ethan.nguyen <ethannguyen@thehutgroup.com>
2016-12-02 16:49:16 +0000

    add media

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,11 +6,23 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className="homeBody">
-        <WidgetBackground vimeoId="156296154"
+        <WidgetBackground class="widgetBackground-mainVideo"
+                          vimeoId="156296154"
                           image="https://static1.squarespace.com/static/5300d507e4b0fa8137bdc65d/t/57504f474c2f85c0e9462d36/1464880969849/devon-stank-squarespace-developer-header-01.jpg?format=2500w"
                           highlight="Lorem ipsum dolor sit amet, ligula amet duis sed praesent libero dui"
                           title="Lorem ipsum dolor sit amet"
                           buttonText="Lorem ipsum"
+                          buttonLink="#"
+        />
+        <WidgetBackground class="widgetBackground-intro"
+                          title="Harrisburg Web Design & Squarespace Development"
+                          text="Being that I only work with a select amount of clients at once, I’m able to provide agency service, without the agency price.  I enjoy working with local businesses within the Harrisburg area, as well as some of my previous tech startups stretching as far as Spain to London. Whether you're looking to improve your current website or completely start fresh, it's worth a quick conversation to see what I could offer you."
+        />
+        <WidgetBackground class="widgetBackground-more"
+                          title="Lorem ipsum dolor sit amet, ligula amet"
+                          text="Being that I only work with a select amount of clients at once, I’m able to provide agency service, without the agency price.  I enjoy working with local businesses within the Harrisburg area, as well as some of my previous tech startups stretching as far as Spain to London. "
+                          mediaVimeoId="156296154"
+                          buttonText='More About Me'
                           buttonLink="#"
         />
       </div>
