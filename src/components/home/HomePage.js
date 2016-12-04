83bc57aa9ed8fb3a8b273ec5a0b04cbe1fcceb0c
Ethan <ethan.nguyens@gmail.com>
2016-12-04 21:57:59 +0000

    add variables and grid system

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,11 +5,11 @@ import WidgetBackground from '../widgetBackground/WidgetBackground';
 class HomePage extends React.Component {
   render() {
     return (
-      <div className="homeBody">
+      <div className="Grid-cell homeBody">
         <WidgetBackground class="widgetBackground-mainVideo"
                           vimeoId="156296154"
                           image="https://static1.squarespace.com/static/5300d507e4b0fa8137bdc65d/t/57504f474c2f85c0e9462d36/1464880969849/devon-stank-squarespace-developer-header-01.jpg?format=2500w"
-                          highlight="I create problems and then seeking the best answers"
+                          highlight="I create problems. I seek answers. I take actions. I am a doer"
                           title="Beautiful & Powerful Building the Webapp"
                           buttonText="Perfection"
                           buttonLink="#"
