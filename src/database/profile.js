a7394cb8710dd7c71a8d66cbf3927268b0d39e79
Ethan Nguyen <ethan.nguyens@gmail.com>
2016-12-13 12:02:40 +0000

    add mongoose

--- src/database/profile.js
+++ src/database/profile.js
@@ -2,6 +2,9 @@ const Profile = {
   instagram: {
     clientId: '652628113c97413bbb45bb16423d4c10',
     clientSecret: '30bffd20ecdd41d694582c8822a50752'
+  },
+  mongo: {
+    uri: process.env.MONGOLAB_URI || 'mongodb://ethannguyens:Eth@N240991@ds133438.mlab.com:33438/heroku_5wbk6xft'
   }
 };
 
