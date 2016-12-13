f177b92aa18b9dba4369af845c6c778200b7cf11
Ethan Nguyen <ethan.nguyens@gmail.com>
2016-12-13 17:28:43 +0000

    webpack config change

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -34,7 +34,7 @@ export default {
     loaders: [
       {test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"]},
       {test: /\.jsx?$/, include: path.resolve(__dirname, 'src'), loaders: ['babel']},
-      { test: /\.json$/, loaders:["json-loader"] },
+      {test: /\.json$/, loader: "json"},
       {test: /(\.css)$/, loader: ExtractTextPlugin.extract("css?sourceMap")},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
       {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
