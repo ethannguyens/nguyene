bdddfbb57f0a2fb6fa41af2cedc04a89d90ea5b7
ethan.nguyen <ethannguyen@thehutgroup.com>
2017-01-24 17:25:56 +0000

    add header and footer back

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
 import Header from './header/Header';
-import Footer from './common/Footer';
+import Footer from './footer/Footer';
 import {connect} from 'react-redux';
 
 const database = require('../database/database');
 
 class App extends React.Component {
+  constructor() {
+    this.menu = [{
+      id: 'home',
+      link: '/home',
+      text: 'Home'
+    }];
+  }
+
   render() {
     return (
       <div className="">
+        <Header menu=`${this.menu}`/>
         {this.props.children}
       </div>
     );
