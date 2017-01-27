b6cc04d0653e22fc9d5dbc6bbdc972ac3b1a60eb
Ethan Nguyen <ethan.nguyens@gmail.com>
2017-01-27 11:15:54 +0000

    add footer database

--- src/database/database.js
+++ src/database/database.js
@@ -36,6 +36,55 @@ const menu = [
   }
 ];
 
+const footer = [{
+    id: "portfolio",
+    link: "portfolio",
+    text: "portfolio"
+  },
+  {
+    id: "education",
+    link: "education",
+    text: "education"
+  },
+  {
+    id: "about",
+    link: "about",
+    text: "about"
+  },
+  {
+    id: "blog",
+    link: "blog",
+    text: "blog"
+  },
+  {
+    id: "resume",
+    link: "resume",
+    text: "resume"
+  },
+  {
+    id: "contact",
+    link: "contact",
+    text: "contact"
+  }];
+
+const term = [{
+  term: 'Term of Services,',
+  privacy: 'Privacy Policy',
+  copyright: 'Copyright Policy'
+}];
+
+const social = [{
+  facebook: 'facebook',
+  twitter: 'twitter',
+  instagram: 'instagram',
+  github: 'github',
+  quora: 'quora'
+}];
+
+
 module.exports = {
-  menu: menu
+  menu: menu,
+  footer: footer,
+  term: term,
+  social: social
 };
