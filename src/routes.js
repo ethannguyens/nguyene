950c6c8cdc564914997b47fb190a3f9f545f33b6
ethan.nguyen <ethan.nguyens@gmail.com>
2017-02-01 16:05:16 +0000

    add engines

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from './components/App';
-import HomePage from './components/home/HomePage';
+import App from './components/App.jsx';
+import HomePage from './components/home/HomePage.jsx';
 
 export default (
   <Route path="/" component={App}>
