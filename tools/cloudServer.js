d3662e62717ff70d42499b3751a88ac96cef2ada
ethan.nguyen <ethan.nguyens@gmail.com>
2017-02-02 11:42:12 +0000

    add cloud server

---
+++ tools/cloudServer.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var compression = require('compression');
+
+var port = process.env.port || 3000;
+var app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server started port: ${port}`);
+  }
+});
